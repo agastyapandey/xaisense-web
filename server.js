7967cefe5ec312a546c1a31c6fd29a7d205b4188
Agastya Pandey <pandey.agst@gmail.com>
2018-09-18 00:04:07 +0530

    routing added for other pages[C

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ app.use('/assets', express.static('assets'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
+app.get('/services', function (req, res) {
+  res.sendFile(path.join(__dirname + '/services.html'));
+})
+app.get('about/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/about.html'));
+})
+app.get('portfolio/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/portfolio.html'));
+})
+app.get('pricing/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'pricing.html'));
+})
+app.get('contact/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'contact.html'));
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
